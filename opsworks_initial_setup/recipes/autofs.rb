23e4b393e97d30f89e6fa2855966e82761eeea63
Mike Prosper <mprosper@casenex.com>
2014-01-03 16:23:32 -0800

    Removed addition of line to auto.master
    This forces autofs to check volumes each time the instance is started.

--- opsworks_initial_setup/recipes/autofs.rb
+++ opsworks_initial_setup/recipes/autofs.rb
@@ -13,14 +13,14 @@ template node[:opsworks_initial_setup][:autofs_map_file] do
 end
 
 ruby_block "Update autofs configuration" do
-  block do
-    handle_to_master = Chef::Util::FileEdit.new("/etc/auto.master")
-    handle_to_master.insert_line_if_no_match(
-      node[:opsworks_initial_setup][:autofs_map_file],
-      "/- #{node[:opsworks_initial_setup][:autofs_map_file]} -t 3600"
-    )
-    handle_to_master.write_file
-  end
+  #block do
+    #handle_to_master = Chef::Util::FileEdit.new("/etc/auto.master")
+    #handle_to_master.insert_line_if_no_match(
+    #  node[:opsworks_initial_setup][:autofs_map_file],
+    #  "/- #{node[:opsworks_initial_setup][:autofs_map_file]} -t 3600"
+    #)
+    #handle_to_master.write_file
+  #end
   notifies :restart, "service[autofs]", :immediately
   not_if { ::File.read('/etc/auto.master').include?('auto.opsworks') }
 end
