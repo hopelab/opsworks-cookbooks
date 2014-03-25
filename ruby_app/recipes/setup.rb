10ad58e6cbd6b03410450a10b19d3ec93c989199
Yuko Takegoshi <ytakegoshi@casenex.com>
2014-03-25 16:44:20 -0700

    Added setup.rb, deploy.rb changed so that it works up through bundle install

---
+++ ruby_app/recipes/setup.rb
@@ -0,0 +1,34 @@
+bash "update apt-get" do
+	code <<-EOH
+							sudo apt-get update
+	EOH
+	action :run
+end
+
+bash "install nginx" do
+	code <<-EOH
+							sudo apt-get install nginx
+	EOH
+	action :run
+end
+
+bash "install freetds" do
+	code <<-EOH
+							sudo apt-get install -y freetds-dev
+	EOH
+	action :run
+end
+
+bash "install tinytds" do
+	code <<-EOH
+							sudo gem install tiny_tds
+	EOH
+	action:run
+end
+
+bash "install sqlite3" do
+	code <<-EOH
+							sudo apt-get install libsqlite3-dev
+	EOH
+	action :run
+end
