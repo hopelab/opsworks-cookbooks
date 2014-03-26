19e2f8868385abb8623debd33a8cbf45a79724ee
Yuko Takegoshi <ytakegoshi@casenex.com>
2014-03-26 16:01:32 -0700

    start_command paths changed

--- ruby_app/attributes/configure.rb
+++ ruby_app/attributes/configure.rb
@@ -4,6 +4,6 @@ include_attribute "deploy::default"
 default[:opsworks][:rack_stack][:name] = "etl_app"
 default[:opsworks][:rack_stack][:needs_reload] = true
 default[:opsworks][:rack_stack][:service] = 'etl_app'
-default[:opsworks][:rack_stack][:start_command] = "/srv/www/etl_app/bin/etl -d -P /srv/www/etl_app/run/etl.pid -l /srv/www/etl_app/shared/log/etl.log"
+default[:opsworks][:rack_stack][:start_command] = "bin/etl -d -P run/etl.pid -l shared/log/etl.log"
 default[:opsworks][:rack_stack][:bundle_command] = "/usr/local/bin/bundle" # "/usr/local/rvm/gems/ruby-1.9.3-p327@global/bin/bundle"
 
