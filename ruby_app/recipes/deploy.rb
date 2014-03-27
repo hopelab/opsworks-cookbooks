f2e87a1cdebe19a273b28dfdc2954d9fcfea363f
Yuko Takegoshi <ytakegoshi@casenex.com>
2014-03-27 14:02:33 -0700

    Quotes added to monit config file echo

--- ruby_app/recipes/deploy.rb
+++ ruby_app/recipes/deploy.rb
@@ -143,8 +143,8 @@ node[:deploy].each do |application, _|
 		bash "Adding etl to monit" do
 			code <<-EOH
 			sudo echo "check process etl_app with pidfile /srv/www/etl_app/current/run/etl.pid
-start program = "/srv/www/etl_app/current/bin/etl -d -P /srv/www/etl_app/current/run/etl.pid -l /srv/www/etl_app/current/shared/log/etl.log"
-stop program = "/srv/www/etl_app/current/bin/etl -k -P /srv/www/etl_app/current/run/etl.pid"" >> /etc/monit/monitrc
+start program = \"/srv/www/etl_app/current/bin/etl -d -P /srv/www/etl_app/current/run/etl.pid -l /srv/www/etl_app/current/shared/log/etl.log\"
+stop program = \"/srv/www/etl_app/current/bin/etl -k -P /srv/www/etl_app/current/run/etl.pid\"" >> /etc/monit/monitrc
 			EOH
 		end
 	end
